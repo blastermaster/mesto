a1d6e41b6d399c3d20c066eb979eb0e725693504
thisisartorius <artur.poe@gmail.com>
2021-04-20 12:41:49 -0400

    fix: Исправления после 1 ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const openPopupButton = document.querySelector('.profile__button_action_edit');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close-btn')
+let openPopupButton = document.querySelector('.profile__button_action_edit');
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__close-btn')
 let userName = document.querySelector('.profile__name')
 let inputName = document.querySelector('.change-form__input_name_value');
 let userInfo = document.querySelector('.profile__about')
@@ -8,16 +8,20 @@ let inputInfo = document.querySelector('.change-form__input_info_value');
 let formElement = document.querySelector('.change-form')
 
 
-function togglePopup(event) {
-    popup.classList.toggle('popup__opened');
+//Функция открывает форму и берет значения из профиля
+function popupOpen() {
+    popup.classList.add('popup_opened');
     inputName.value = userName.textContent;
     inputInfo.value = userInfo.textContent;
 }
+//Функция закрывает форму
+function popupClose() {
+    popup.classList.remove('popup_opened')
+}
 
-openPopupButton.addEventListener('click', togglePopup)
-closePopupButton.addEventListener('click', togglePopup)
 
 
+//Функция отправляет форму
 function formSubmitHandler(evt) {
     evt.preventDefault();
     userName.textContent = inputName.value;
@@ -25,4 +29,7 @@ function formSubmitHandler(evt) {
     togglePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+
+formElement.addEventListener('submit', formSubmitHandler); //отправка формы
+openPopupButton.addEventListener('click', popupOpen); //открывает форму и заполняет инпуты
+closePopupButton.addEventListener('click', popupClose); // закрывает форму
