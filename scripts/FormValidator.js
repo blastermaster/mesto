ce55289248b775b9e7f88ac95c708b2525a7fd4e
thisisartorius <artur.poe@gmail.com>
2021-07-26 13:07:27 -0400

    Update FormValidator.js

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,3 +1,10 @@
 export default class FormValidator {
-
+    constructor(configSelectors, formElement) {
+        this._inputSelector = configSelectors.inputSelector;
+        this._submitButtonSelector = configSelectors.submitButtonSelector;
+        this._inactiveButtonClass = configSelectors.inactiveButtonClass;
+        this._inputErrorClass = configSelectors.inputErrorClass;
+        this._errorClass = configSelectors.errorClass;
+        this._formElement = formElement;
+    }
 }
